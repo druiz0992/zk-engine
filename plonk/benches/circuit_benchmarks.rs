d16272a5c8667312fc88cdccdacc59cb6112a1c5
Ilyas Ridhuan <ilyas@torus.io>
2023-09-05 23:18:07 +0100

    Add mint circuit, tests and benchmarks

--- plonk/benches/circuit_benchmarks.rs
+++ plonk/benches/circuit_benchmarks.rs
@@ -1,7 +1,11 @@
-use ark_ed_on_bn254::Fq;
+use ark_ec::{AffineRepr, CurveGroup};
+use ark_ed_on_bn254::{EdwardsAffine, Fq, Fr};
 use criterion::{criterion_group, criterion_main, Criterion};
 use jf_relation::{Circuit, PlonkCircuit};
-use plonk::primitives::circuits::poseidon::{PoseidonGadget, PoseidonStateVar};
+use plonk::{
+    client::circuits::mint::mint_circuit,
+    primitives::circuits::poseidon::{PoseidonGadget, PoseidonStateVar},
+};
 use std::str::FromStr;
 
 pub fn benchmark_poseidon(c: &mut Criterion) {
@@ -40,5 +44,42 @@ pub fn benchmark_poseidon(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_poseidon);
+pub fn benchmark_mint(c: &mut Criterion) {
+    c.bench_function("Mint Circuit - 1 Output", |b| {
+        b.iter(|| {
+            let value = Fq::from_str("1").unwrap();
+            let token_id = Fq::from_str("2").unwrap();
+            let token_nonce = Fq::from_str("3").unwrap();
+            let token_owner =
+                (EdwardsAffine::generator() * Fr::from_str("4").unwrap()).into_affine();
+            let mut circuit = mint_circuit::<ark_ed_on_bn254::EdwardsConfig, ark_bn254::Bn254, 1>(
+                [value],
+                [token_id],
+                [token_nonce],
+                token_owner,
+            )
+            .unwrap();
+            circuit.finalize_for_arithmetization().unwrap();
+        })
+    });
+    c.bench_function("Mint Circuit - 2 Output", |b| {
+        b.iter(|| {
+            let value = Fq::from_str("1").unwrap();
+            let token_id = Fq::from_str("2").unwrap();
+            let token_nonce = Fq::from_str("3").unwrap();
+            let token_owner =
+                (EdwardsAffine::generator() * Fr::from_str("4").unwrap()).into_affine();
+            let mut circuit = mint_circuit::<ark_ed_on_bn254::EdwardsConfig, ark_bn254::Bn254, 2>(
+                [value, value],
+                [token_id, token_id],
+                [token_nonce, token_nonce],
+                token_owner,
+            )
+            .unwrap();
+            circuit.finalize_for_arithmetization().unwrap();
+        })
+    });
+}
+
+criterion_group!(benches, benchmark_poseidon, benchmark_mint);
 criterion_main!(benches);
